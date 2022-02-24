d7a325c8179b0d57f404cb4d1742e201605f3bb1
Ninaxtrem <nina@starxtrem.com>
2022-02-25 00:03:46 +0100

    grid

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <title>Vector</title>
+
+<!-- partie choix de jeux -->
+
+<main class="main">
+
+    <article class="box1">
+    <img src="assets/img/jeu_pvp.png">
+    <div class="text">
+      <div class="h2">Survie<br> <br> </div>
+      <div class="p">Le mode Robinson Crusoé, ici le but<br>  
+          sera tout simplement<br> 
+          pour le joueur de survivre.<br>  
+      </div>
+    </div>
+    </article>
+
+    <article class="box2">
+    <img src="assets/img/jeu_construction.png">
+    <div class="text">
+    <div class="h2">Construction<br> <br> </div>
+    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
+          sera tout simplement<br> 
+          pour le joueur de survivre.<br>
+    </div>
+    </article>
+
+
+    <article class="box3">
+    <img src="assets/img/jeu_pictionary.png">
+    <div class="text">
+    <div class="h2">Pictionary<br> <br> </div>
+    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
+          sera tout simplement<br> 
+          pour le joueur de survivre.<br>
+    </div>      
+    </article>
+
+
+    <article class="box4">
+    <img src="assets/img/jeu_discofloor.png">
+    <div class="text">
+    <div class="h2">Disco Floor<br> <br> </div>
+    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
+          sera tout simplement<br> 
+          pour le joueur de survivre.<br>
+    </div>
+    </article>
+
+
+    <article class="box5">
+    <img src="assets/img/jeu_bedwars.png">
+    <div class="text">
+    <div class="h2">Bedwars<br> <br> </div>
+    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
+          sera tout simplement<br> 
+          pour le joueur de survivre.<br>
+          </div>
+          </div>
+    </article>
+
+
+    <article class="box6">
+    <img src="assets/img/jeu_pvp.png">
+    <div class="text">
+    <div class="h2">Hardcorp<br> <br> </div>
+    <div class="p">Le mode Robinson Crusoé, ici le but 
+          sera tout simplement
+          pour le joueur de survivre.
+    </div>
+    </article>
+
+  </main>
+
+</div>
+
+
+
+
+</head>
+<body>
+    
+</body>
+</html>
