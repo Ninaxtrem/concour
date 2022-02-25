b61a9afbafeddd03df101dd4d0ef2a9b53964a2e
Ninaxtrem <nina@starxtrem.com>
2022-02-25 11:38:58 +0100

    bouton

--- index.php
+++ index.php
@@ -11,125 +11,35 @@
     <title>Vector</title>
 
 <!-- partie choix de jeux -->
-
-<main class="main">
-
-    <article class="box1">
-    <img src="assets/img/jeu_pvp.png">
-    <div class="text">
-      <div class="h2">Survie<br> <br> </div>
-      <div class="p">Le mode Robinson Crusoé, ici le but<br>  
-          sera tout simplement<br> 
-          pour le joueur de survivre.<br>  
-      </div>
-    </div>
-    <div class="container-btn">
-        <a href="#" class="btn btn-1">
-            <svg width="277" height="62">
-                <rect class="btn-telecharger" x="5" y="5" rx="25" fill="none" stroke="#44C3D9" width="266" height="50"></rect>
-            </svg>
-            <span>Inscription</span>
-        </a>
-    </div>
-    </article>
-
-    <article class="box2">
-    <img src="assets/img/jeu_construction.png">
-    <div class="text">
-    <div class="h2">Construction<br> <br> </div>
-    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
-          sera tout simplement<br> 
-          pour le joueur de survivre.<br>
-    </div>
-    <div class="container-btn">
-        <a href="#" class="btn btn-1">
-            <svg width="277" height="62">
-                <rect class="btn-telecharger" x="5" y="5" rx="25" fill="none" stroke="#44C3D9" width="266" height="50"></rect>
-            </svg>
-            <span>Inscription</span>
-        </a>
-    </div>
-    </article>
-
-
-    <article class="box3">
-    <img src="assets/img/jeu_pictionary.png">
-    <div class="text">
-    <div class="h2">Pictionary<br> <br> </div>
-    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
-          sera tout simplement<br> 
-          pour le joueur de survivre.<br>
-    </div>    
-    <div class="container-btn">
-        <a href="#" class="btn btn-1">
-            <svg width="277" height="62">
-                <rect class="btn-telecharger" x="5" y="5" rx="25" fill="none" stroke="#44C3D9" width="266" height="50"></rect>
-            </svg>
-            <span>Inscription</span>
-        </a>
-    </div>  
-    </article>
-
-
-    <article class="box4">
-    <img src="assets/img/jeu_discofloor.png">
-    <div class="text">
-    <div class="h2">Disco Floor<br> <br> </div>
-    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
-          sera tout simplement<br> 
-          pour le joueur de survivre.<br>
-    </div>
-    <div class="container-btn">
-        <a href="#" class="btn btn-1">
-            <svg width="277" height="62">
-                <rect class="btn-telecharger" x="5" y="5" rx="25" fill="none" stroke="#44C3D9" width="266" height="50"></rect>
-            </svg>
-            <span>Inscription</span>
-        </a>
-    </div>
-    </article>
-
-
-    <article class="box5">
-    <img src="assets/img/jeu_bedwars.png">
-    <div class="text">
-    <div class="h2">Bedwars<br> <br> </div>
-    <div class="p">Le mode Robinson Crusoé, ici le but<br>  
-          sera tout simplement<br> 
-          pour le joueur de survivre.<br>
-          </div>
-          </div>
-          <div class="container-btn">
-        <a href="#" class="btn btn-1">
-            <svg width="277" height="62">
-                <rect class="btn-telecharger" x="5" y="5" rx="25" fill="none" stroke="#44C3D9" width="266" height="50"></rect>
-            </svg>
-            <span>Inscription</span>
-        </a>
-    </div>
-    </article>
-
-
-    <article class="box6">
-    <img src="assets/img/jeu_pvp.png">
-    <div class="text">
-    <div class="h2">Hardcorp<br> <br> </div>
-    <div class="p">Le mode Robinson Crusoé, ici le but 
-          sera tout simplement
-          pour le joueur de survivre.
-    </div>
-    <div class="container-btn">
-        <a href="#" class="btn btn-1">
-            <svg width="277" height="62">
-                <rect class="btn-telecharger" x="5" y="5" rx="25" fill="none" stroke="#44C3D9" width="266" height="50"></rect>
-            </svg>
-            <span>Inscription</span>
-        </a>
-    </div>
-    </article>
-
-  </main>
-
+<div class="wrapper">
+  <div class="news-item hero-item">
+  <div class="boutona">
+  <p>
+   <a href="#">Jouez</a>
+ </p>
+</div>
+  </div>
+  <div class="news-item standard-item">
+  <div class="boutonb">
+  <p>
+   <a href="#">Jouez</a>
+ </p>
+</div>
+  </div>
+  <div class="news-item standard-item">
+  <div class="boutonc">
+  <p>
+   <a href="#">Jouez</a>
+ </p>
+</div>
+  </div>
+  <div class="news-item standard-item">
+  <div class="boutond">
+  <p>
+   <a href="#">Jouez</a>
+ </p>
+</div>
+  </div>
 </div>
 <!-- fin partie choix de jeux -->
 <!-- page type -->
